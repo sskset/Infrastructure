5fd7d7869a512093cae760d100341c5fca8f5685
sskset@gmail.com <sskset@gmail.com>
2017-08-22 15:30:46 +1000

    Create Infrastructure project and add Infrastructure.Specification project

---
+++ Infrastructure.Specification/Infrastructure.Specification/Specification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Specification
+{
+    public abstract class Specification<T>
+    {
+        public static readonly Specification<T> All = new IdentitySpecification<T>();
+
+        public bool IsSatisfiedBy(T entity)
+        {
+            Func<T, bool> predicate = ToExpression().Compile();
+            return predicate(entity);
+        }
+
+        public abstract Expression<Func<T, bool>> ToExpression();
+    }
+}
